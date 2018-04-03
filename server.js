e38ac4228f724ee4cd9e6648a49f2bd33a4f90aa
unknown <trnam2811@gmail.com>
2018-04-03 13:08:55 -0700

    committing

--- server.js
+++ server.js
@@ -5,13 +5,37 @@ const port = process.env.PORT || 8080;
 
 var app = express();
 
-hbs.registerPartials(__dirname + '/public/partials');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+})
 
 app.get('/', (request, response) => {
-	response.send('about.hbs')
+	response.render('login.hbs', {
+		title: 'Login page'
+	})
+});
+
+app.get('/signup', (request, response) => {
+	response.render('signup.hbs', {
+		title: 'Sign up'
+	})
+});
+
+app.get('/mainpage', (request, response) => {
+	response.render('mainpage.hbs', {
+		title: 'Main page'
+	})
+});
+
+app.get('/mytracks', (request, response) => {
+	response.render('mytracks.hbs', {
+		title: 'My tracks'
+	})
 });
 
 app.listen(port, () => {
