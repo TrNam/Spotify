d8a0544fedfc4c4e483b59cec198cebcf73c7cde
unknown <trnam2811@gmail.com>
2018-04-06 10:33:54 -0700

    changing sign up page

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const request = require('request');
 const hbs = require('hbs');
 const fs = require('fs');
 const port = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
 
 var app = express();
+
+// app.use(bodyParser); 
+// app.use(bodyParser.urlencoded({    
+//   extended: true
+// })); 
+
+// app.use(express.json());
+// app.use(express.urlencoded());
 
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -13,6 +23,7 @@ app.use(express.static(__dirname + '/views'));
 hbs.registerHelper('getCurrentYear', () => {
 	return new Date().getFullYear();
 })
+
 
 app.get('/', (request, response) => {
 	response.render('login.hbs', {
@@ -25,6 +36,15 @@ app.get('/signup', (request, response) => {
 		title: 'Sign up'
 	})
 });
+
+// app.get('/signup', (request, response) => {
+// 	var users = response.end(JSON.stringify(request.body))
+// 	fs.appendFile('users.json', users, function(err){
+// 		if(err){
+// 			console.log('Unable to write to file!')
+// 		};
+// 	};
+// });
 
 app.get('/mainpage', (request, response) => {
 	response.render('mainpage.hbs', {
@@ -39,6 +59,7 @@ app.get('/mytracks', (request, response) => {
 });
 
 app.get('/congratulations', (request, response) => {
+	// console.log(request.body)
 	response.render('congratulations.hbs', {
 		title: 'Congratulations'
 	})
